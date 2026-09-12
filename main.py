8ce178ce5274bad5a45e1f10b648f5619cdaf91b
cupcakegirl2026 <liamnyaks@gmail.com>
2026-09-12 09:47:09 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+name="Ethel"
+age=10
+height=146.5
+is_student=True
+
+print(f"Name:{name}({type(name)})")
+print(f"Age:{age}({type(age)})")
+print(f"Height:{height}({type(height)})")
+print(f"Is Student:{is_student}({type(is_student)})")
